ae8ff6360ba4980c17850c250a9a8b4a2ebc9a3f
Alex Parinov <creafz@gmail.com>
2017-10-22 10:48:23 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for i in $(seq 0 5)
 do
     python train.py --experiment-name se_refinenet_1024 --model-name se_refinenet_1024 --fold-num "$i"
-    python predict.py --experiment-name se_refinenet_1024 --model-name refinenet_1024 --fold-num "$i" --dirname se_refinenet_1024
+    python predict.py --experiment-name se_refinenet_1024 --model-name se_refinenet_1024 --fold-num "$i" --dirname se_refinenet_1024
 done
 
 python make_submission.py --dirname se_refinenet_1024 --submission-filename se_refinenet_1024
